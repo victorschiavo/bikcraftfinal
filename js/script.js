4e388fd7cca6e45e604536461ab799d256587867
Victor Schiavo <victorschiavo@outlook.com.br>
2024-01-22 18:28:12 -0300

    ativar parametros url

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar links menu ativo
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -9,3 +10,17 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar itens do orçamento 
+const parametro = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if (elemento) {
+        elemento.checked = true;
+    }
+    // console.log(elemento);
+}
+
+parametro.forEach(ativarProduto);
+
